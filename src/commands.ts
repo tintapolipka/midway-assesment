d296a2820862e7feab28a1876b84df046e47b29e
tintapolipka <109940436+tintapolipka@users.noreply.github.com>
2024-10-21 16:55:36 +0200

    details implemented

--- src/commands.ts
+++ src/commands.ts
@@ -15,3 +15,20 @@ export async function list(store: Store<RecipeType[]>, args: string[]) {
   console.log('Your recipes:');
   console.log(formatted);
 }
+
+export async function details(store: Store<RecipeType[]>, args: string[]) {
+
+  if(!args || args.length !==1 ){
+    throw new AppError("The details command should have one numeric ID argument.")
+  }
+const id = parseInt(args[0]);
+
+const recipe = new Recipe(store);
+const recipes = await recipe.readAll();
+const filteredRecipes = recipes.filter(rec=>rec.id === id);
+const formatted = 
+ `ID: ${filteredRecipes[0].id}\nName: ${filteredRecipes[0].name}`
+
+  console.log('Your recipe:');
+console.log(formatted);
+}

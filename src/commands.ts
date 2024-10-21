03f089a010f12470aed52be1f2e28d96371e8224
tintapolipka <109940436+tintapolipka@users.noreply.github.com>
2024-10-21 16:46:27 +0200

    list-argument-overload-error-added

--- src/commands.ts
+++ src/commands.ts
@@ -1,7 +1,12 @@
+import { AppError } from "./app.error";
 import { Recipe, RecipeType } from "./recipe";
 import { Store } from "./stores/store.type";
 
 export async function list(store: Store<RecipeType[]>, args: string[]) {
+  if(args && args.length){
+    throw new AppError("The list command should not have any argument.")
+  }
+  
   const recipe = new Recipe(store);
   const recipes = await recipe.readAll();
   const formatted = recipes
